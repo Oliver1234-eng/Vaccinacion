14b2cae9f0bdc0c1cd0c8dde2d816b4105b6f16d
Oliver1234-eng <oliver.kojic22@gmail.com>
2021-12-15 10:35:14 +0100

    Pocetni projekat

---
+++ SlozeniOblikVezbiProjekat/src/main/java/com/ftn/SlozeniOblikVezbiProjekat/SlozeniOblikVezbiProjekatApplication.java
@@ -0,0 +1,13 @@
+package com.ftn.SlozeniOblikVezbiProjekat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlozeniOblikVezbiProjekatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlozeniOblikVezbiProjekatApplication.class, args);
+	}
+
+}
---
+++ SlozeniOblikVezbiProjekat/src/test/java/com/ftn/SlozeniOblikVezbiProjekat/SlozeniOblikVezbiProjekatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.SlozeniOblikVezbiProjekat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlozeniOblikVezbiProjekatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
